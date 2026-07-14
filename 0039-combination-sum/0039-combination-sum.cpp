66c3385b1a8777c7ce61ae01a50d2c481f99dd93
DeathBlade07 <227273276+deathblade007@users.noreply.github.com>
2026-07-14 12:05:18 +0530

    Time: 70 ms (9.67%), Space: 65.7 MB (8.27%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void sub(vector<int> arr, vector<int>& al, int i, int k, int& sum) {
+        if (i == arr.size() || sum > k) {
+            return;
+        }
+        if (sum == k) {
+            ans.push_back(al);
+            return;
+        } else {
+            al.push_back(arr[i]);
+            sum += arr[i];
+            sub(arr, al, i, k, sum);
+            al.pop_back();
+            sum -= arr[i];
+            sub(arr, al, i + 1, k, sum);
+        }
+        return;
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> al;
+        int sum = 0;
+        sub(candidates, al, 0, target, sum);
+        return ans;
+    }
+};
